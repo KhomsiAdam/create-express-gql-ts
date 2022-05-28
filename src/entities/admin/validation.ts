9e148eca6475452dbf2826d1f7d95e0b4f3d5926
KhomsiAdam <khomsiadam@gmail.com>
2022-05-29 00:18:55 +0100

    refactor: removed alphanum from validation

--- src/entities/admin/validation.ts
+++ src/entities/admin/validation.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
 export const adminSchema = Joi.object({
-  firstname: Joi.string().alphanum().trim(),
-  lastname: Joi.string().alphanum().trim(),
+  firstname: Joi.string().trim(),
+  lastname: Joi.string().trim(),
 });
--- src/tasks/templates/user/validation.ts
+++ src/tasks/templates/user/validation.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
 export const {{lowercaseName}}Schema = Joi.object({
-  firstname: Joi.string().alphanum().trim(),
-  lastname: Joi.string().alphanum().trim(),
+  firstname: Joi.string().trim(),
+  lastname: Joi.string().trim(),
 });
